7bcfa5a3c416b656b2ae024391ed2b2467fa8d70
Elizabeth Dale <eanndale@users.noreply.github.com>
2017-07-28 20:01:54 -0400

    Quote changes

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
  window.onload = function() {
       var quotes = [
-          "\"The worst you can do is fail\"",
+          "\"Girls we are human beings. We crack, we fall, and we bleed. But we always, always stand back up.\"",
           "\"Find your passion\"",
           "\"Just live. Be strong. Move on.\"",
           "\"We create our own deamons\" -Tony Stark"
