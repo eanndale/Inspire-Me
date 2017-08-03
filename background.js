1046e86ca09f04a1c5d48d72d1bbfb842ec0ce6a
Elizabeth Dale <eanndale@users.noreply.github.com>
2017-08-03 10:42:30 -0400

    Test opening url in new tab

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(activeTab){
-  chrome.tabs.executeScript(null, {file: "popup.js"});
+  //chrome.tabs.executeScript(null, {file: "popup.js"});
+  var newURL = "http://stackoverflow.com/";
+  chrome.tabs.create({ url: newURL });
 });
