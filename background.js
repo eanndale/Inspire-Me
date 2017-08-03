d54ed8ef2e556f2f35c92303806a7e4266634393
Elizabeth Dale <eanndale@users.noreply.github.com>
2017-08-03 10:50:09 -0400

    Enable popup.js in new tab

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(activeTab){
-  //chrome.tabs.executeScript(null, {file: "popup.js"});
-  var newURL = "http://stackoverflow.com/";
-  chrome.tabs.create({ url: newURL });
+  chrome.tabs.executeScript(null, {file: "popup.js"});
+  //var newURL = "http://stackoverflow.com/";
+  //chrome.tabs.create({ url: newURL });
 });
