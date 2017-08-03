1c09ba8b5b3221a5fa43c4566fd8658f949947d1
Elizabeth Dale <eanndale@users.noreply.github.com>
2017-08-03 11:16:59 -0400

    Open html in new tab

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
-  chrome.tabs.executeScript(null, {file: "popup.js"});
-  //var newURL = "http://stackoverflow.com/";
-  //chrome.tabs.create({ url: newURL });
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({'url': chrome.extension.getURL('popup.html'), 'selected': true});
 });
