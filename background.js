71820870c16c6d464f03b2b2ffa142dec586ffc1
Elizabeth Dale <eanndale@users.noreply.github.com>
2017-08-21 13:28:45 -0400

    Change back to opening in new tab

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.reload({'url': chrome.extension.getURL('popup.html'), 'selected': true});
+    chrome.tabs.create({'url': chrome.extension.getURL('popup.html'), 'selected': true});
 });
