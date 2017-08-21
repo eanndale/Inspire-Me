b32f6062c1e2e415d4ed04bfd81fe9be3b462fa5
Elizabeth Dale <eanndale@users.noreply.github.com>
2017-08-21 13:51:27 -0400

    updated syntax

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.browserAction.onClicked.addListener(
     function () {
     chrome.tabs.query({'url': chrome.extension.getURL('popup.html')}, function(tabs) {
-        if(tabs.length) { 
+        if (tabs.length) { 
             chrome.tabs.reload(tabs[0].tabId); 
         }
         else {
             // create new tab
             chrome.tabs.create({'url': chrome.extension.getURL('popup.html'), 'selected': true});
         }
-    }
+    });
 });
