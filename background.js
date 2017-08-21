a29daca157122dc9d958d268a22c410108c2d8a7
Elizabeth Dale <eanndale@users.noreply.github.com>
2017-08-21 13:25:16 -0400

    Add reload instead of create to reload tab

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({'url': chrome.extension.getURL('popup.html'), 'selected': true});
+    chrome.tabs.reload({'url': chrome.extension.getURL('popup.html'), 'selected': true});
 });
