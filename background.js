f8283e3be5d5af46ade3dc798343e45d30be573d
Elizabeth Dale <eanndale@users.noreply.github.com>
2017-08-21 13:34:00 -0400

    Reload page if tab already open

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({'url': chrome.extension.getURL('popup.html'), 'selected': true});
-});
+chrome.browserAction.onClicked.addListener(
+    function () {
+    chrome.tabs.query({'url': chrome.extension.getURL('popup.html')}, function(tabs) {
+        if(tabs.length) { 
+            chrome.tabs.reload(tabs[0].tabId); 
+        }
+        else {
+            // create new tab
+            chrome.tabs.create({'url': chrome.extension.getURL('popup.html'), 'selected': true});
+        }
+    });
